b947a7cda028f5f56be5151d6c7c0c1a55a6e885
ttawang <titan.tib@gmail.com>
2021-12-28 22:24:07 +0700

    tambahan titan

---
+++ public/my-asset/enakin.js
@@ -0,0 +1,22 @@
+/* BUATANKU */
+
+//input pada text menjadi angka
+function number_only(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+
+// pencarian pada select tanpa modal
+$('.select-cari').select2({
+	theme: 'bootstrap4'
+});
+
+// pencarian pada select dengan modal
+$('.select-cari-modal').select2({
+	theme: 'bootstrap4',
+	dropdownParent : $('.modal'),
+});
